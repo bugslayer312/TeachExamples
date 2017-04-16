083a8b0befa9b1449209fc635eac43bd15a3c94d
Eugene Moskalev <bugslayer312@gmail.com>
2017-04-17 00:53:05 +0300

    Pass structures by pointer

--- PointersReferences/main.cpp
+++ PointersReferences/main.cpp
@@ -7,15 +7,25 @@ struct Distance
     float inches;
 };
 
-Distance Summ(Distance d1, Distance d2)
+Distance Summ(Distance const* d1, Distance const* d2)
 {
-    Distance result = {d1.feet + d2.feet, d1.inches + d2.inches};
+    Distance result = {d1->feet + d2->feet, d1->inches + d2->inches};
+    if (result.inches >= 12.0f)
+    {
+        result.feet += 1;
+        result.inches -= 12.0f;
+    }
     return result;
 }
 
-Distance Difference(Distance d1, Distance d2)
+Distance Difference(Distance const* d1, Distance const* d2)
 {
-    Distance result = {d1.feet - d2.feet, d1.inches - d2.inches};
+    Distance result = {d1->feet - d2->feet, d1->inches - d2->inches};
+    if (result.inches < 0)
+    {
+        result.inches += 12.0f;
+        result.feet -= 1;
+    }
     return result;
 }
 
@@ -29,30 +39,30 @@ Distance Read()
     return result;
 }
 
-void Print(Distance value)
+void Print(Distance const* value)
 {
-    std::cout << value.feet << "\'" << value.inches << "\"";
+    std::cout << value->feet << "\'" << value->inches << "\"";
 }
 
 int main()
 {
     Distance d1 = {3, 5.0f};
     Distance d2 = {2, 8.0f};
-    Distance summ = Summ(d1, d2);
-    Distance sub = Difference(d1, d2);
+    Distance summ = Summ(&d1, &d2);
+    Distance sub = Difference(&d1, &d2);
     
-    Print(d1);
+    Print(&d1);
     std::cout << " + ";
-    Print(d2);
+    Print(&d2);
     std::cout << " = ";
-    Print(summ);
+    Print(&summ);
     std::cout << std::endl;
     
-    Print(d1);
+    Print(&d1);
     std::cout << " - ";
-    Print(d2);
+    Print(&d2);
     std::cout << " = ";
-    Print(sub);
+    Print(&sub);
     std::cout << std::endl;
     return 0;
 }
