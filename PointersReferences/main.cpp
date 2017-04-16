de54d3a99f8d3a8291655ed84f51fbe86caffba6
Eugene Moskalev <bugslayer312@gmail.com>
2017-04-16 23:10:14 +0300

    Bubble sort with pointers

---
+++ PointersReferences/main.cpp
@@ -0,0 +1,62 @@
+#include <clocale>
+#include <iostream>
+
+void Swap(int* a, int* b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void PushMaxToTop(int* array, int count)
+{
+    for (int i = 1; i < count; ++i)
+    {
+        int* current = &array[i];
+        int* previous = current - 1;
+        if (*previous > *current)
+        {
+            Swap(previous, current);
+        }
+    }
+}
+
+void BubbleSort(int* array, int count)
+{
+    for (int subCount = count; subCount > 1; --subCount)
+    {
+        PushMaxToTop(array, subCount);
+    }
+}
+
+void ReadArray(int* array, int count)
+{
+    for (int i = 0; i < count; ++i)
+    {
+        std::cout << "Введите число: ";
+        std::cin >> array[i];
+    }
+}
+
+void PrintArray(int* array, int count)
+{
+    for (int i = 0; i < count; ++i)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int const count = 10;
+    int array[count];
+    ReadArray(array, count);
+    std::cout << "Исходный массив:\n";
+    PrintArray(array, count);
+    BubbleSort(array, count);
+    std::cout << "Отсортированный массив:\n";
+    PrintArray(array, count);
+    return 0;
+}
