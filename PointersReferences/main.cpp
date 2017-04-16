b151326fab70da3a486d792fac0d4b051c2265a9
Eugene Moskalev <bugslayer312@gmail.com>
2017-04-17 00:32:24 +0300

    Working with structures: Distance example.

--- PointersReferences/main.cpp
+++ PointersReferences/main.cpp
@@ -1,19 +1,58 @@
-#include "ArrayUtils.h"
-
 #include <clocale>
 #include <iostream>
 
+struct Distance
+{
+    int feet;
+    float inches;
+};
+
+Distance Summ(Distance d1, Distance d2)
+{
+    Distance result = {d1.feet + d2.feet, d1.inches + d2.inches};
+    return result;
+}
+
+Distance Difference(Distance d1, Distance d2)
+{
+    Distance result = {d1.feet - d2.feet, d1.inches - d2.inches};
+    return result;
+}
+
+Distance Read()
+{
+    Distance result;
+    std::cout << "feet: ";
+    std::cin >> result.feet;
+    std::cout << "inches: ";
+    std::cin >> result.inches;
+    return result;
+}
+
+void Print(Distance value)
+{
+    std::cout << value.feet << "\'" << value.inches << "\"";
+}
+
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    int const count = 10;
-    int* array = (int*)malloc(sizeof(int)*count);
-    ReadArray(array, count);
-    std::cout << "Исходный массив:\n";
-    PrintArray(array, count);
-    BubbleSort(array, count);
-    std::cout << "Отсортированный массив:\n";
-    PrintArray(array, count);
-    free(array);
+    Distance d1 = {3, 5.0f};
+    Distance d2 = {2, 8.0f};
+    Distance summ = Summ(d1, d2);
+    Distance sub = Difference(d1, d2);
+    
+    Print(d1);
+    std::cout << " + ";
+    Print(d2);
+    std::cout << " = ";
+    Print(summ);
+    std::cout << std::endl;
+    
+    Print(d1);
+    std::cout << " - ";
+    Print(d2);
+    std::cout << " = ";
+    Print(sub);
+    std::cout << std::endl;
     return 0;
 }
