6c96008054812e09152a762d283815910042a162
Eugene Moskalev <bugslayer312@gmail.com>
2017-04-17 11:07:05 +0300

    Meet with references

--- PointersReferences/main.cpp
+++ PointersReferences/main.cpp
@@ -5,16 +5,30 @@
 
 int main()
 {
-    int const count = 5;
-    Distance* array = new Distance[count];
+    int a = 1;
+    int b = 2;
+
+    int& refa = a;
+    std::cout << "refa = " << refa << std::endl << std::endl;
+
+    refa = 5;
+    std::cout << "refa = " << refa << ", a = " << a << std::endl;
+    std::cout << "pointer to refa = " << &refa << ", pointer to a = " << &a << std::endl << std::endl;
     
-    ReadArray(array, count);
-    std::cout << "Исходный массив:\n";
-    PrintArray(array, count);
-    BubbleSort(array, count);
-    std::cout << "Отсортированный массив:\n";
-    PrintArray(array, count);
+    refa = b;
+    std::cout << "refa = " << refa << ", a = " << a << std::endl;
+    std::cout << "pointer to refa = " << &refa << ", pointer to a = " << &a << std::endl;
+    std::cout << "pointer to b = " << &b << std::endl << std::endl;
     
-    delete[] array;
+    Distance d1 = {2, 6.0f};
+    Distance& refd1 = d1;
+    refd1.feet += 5;
+    std::cout << "d1 = ";
+    Print(&d1);
+    std::cout << ", refd1 = ";
+    Print(&refd1);
+    std::cout << std::endl;
+    std::cout << "pointer to d1 = " << &d1 << ", pointer to refd1 = " << &refd1 << std::endl;
+    
     return 0;
 }
