8a946416920226544cfae1614beff6b9caf0f728
Eugene Moskalev <bugslayer312@gmail.com>
2017-04-24 20:27:50 +0300

    Dynamic structures: Single linked list (update) * Added Sort and Reverse functions * Renamed to SingleLinkedList.h/cpp

--- DynStructures/SingleLinkedList.h
+++ DynStructures/SingleLinkedList.h
@@ -8,12 +8,15 @@ struct ListNode
     struct ListNode* Next;
 };
 
-struct ListNode* Create(struct Data* listData);
+struct ListNode* CreateSingleLinkedList(struct Data* listData);
 void Destroy(struct ListNode* node);
 struct ListNode* GetLastNode(struct ListNode* first);
 struct ListNode* PushBack(struct ListNode* first, struct ListNode* newNode);
 struct ListNode* Insert(struct ListNode* first, struct ListNode* newNode);
 struct ListNode* Remove(struct ListNode* first, struct ListNode* toDelete);
-struct ListNode* Find(struct ListNode* first, DataCompareFunc predicate);
+struct ListNode* Find(struct ListNode* first, DataCompareFunc compare);
 int GetCount(struct ListNode* first);
 void PrintList(struct ListNode* first);
+void SwapData(struct ListNode* node1, struct ListNode* node2);
+void BubbleSort(struct ListNode* first);
+struct ListNode* Reverse(struct ListNode* first);
