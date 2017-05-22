5c895adaa54d88cb0a3a8820eabb69690eca3568
Eugene Moskalev <bugslayer312@gmail.com>
2017-05-22 23:00:18 +0300

    adds Distance class

--- oop/main.cpp
+++ oop/main.cpp
@@ -1,6 +1,8 @@
+#include "Distance.h"
+
 #include <iostream>
 
-// Объявление класса
+/*
 class Distance
 {
 private:
@@ -8,49 +10,31 @@ private:
     float m_inches;
     
 public:
-    int GetFeet();
-    float GetInches();
-    void SetData(int feet, float inches);
-    void PrintData();
-};
-
-// Реализация методов класса
-int Distance::GetFeet()
-{
-    return m_feet;
-}
-float Distance::GetInches()
-{
-    return m_inches;
-}
-void Distance::SetData(int feet, float inches)
-{
-    m_feet = feet;
-    m_inches = inches;
-}
-void Distance::PrintData()
-{
-    std::cout << m_feet << "\'" << m_inches << "\"" << std::endl;
-}
-
-struct Distance2
-{
-    // по умолчанию открытый доступ
-    int Feet;
-    float Inches;
-    
-    void PrintData()
+    int GetFeet()
     {
-        std::cout << Feet << "\'" << Inches << "\"" << std::endl;
-    }
+        return m_feet;
+    };
+    float GetInches()
+    {
+        return m_inches;
+    };
+    void SetData(int feet, float inches)
+    {
+        m_feet = feet;
+        m_inches = inches;
+    };
+    void Read()
+    {
+        std::cin >> m_feet >> m_inches;
+    };
+    void Print()
+    {
+        std::cout << m_feet << "\'" << m_inches << "\"" << std::endl;
+    };
 };
+*/
 
 int main()
 {
-    Distance d; // создается d - объект класса Distance
-    d.SetData(5, 2.0f);
-    // d.m_feet = 4; ЗАПРЕЩЕНО, т. к. закрыт доступ
-    d.PrintData();
-    std::cout << "Feet count = " << d.GetFeet() << std::endl;
     return 0;
 }
